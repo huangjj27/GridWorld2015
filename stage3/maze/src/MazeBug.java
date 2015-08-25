59d57ae2eb54066f0244651574e3ca4f4cc7314b
DingCao <349373001@qq.com>
2015-08-25 21:43:23 +0800

    mazeBug uses dps to cross a maze.Randomly selected a direction to go.
    Signed-off-by: DingCao <349373001@qq.com>

---
+++ stage3/maze/src/MazeBug.java
@@ -0,0 +1,187 @@
+package info.gridworld.maze;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.*;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
+/**
+ * A <code>MazeBug</code> can find its way in a maze. <br />
+ * The implementation of this class is testable on the AP CS A and AB exams.
+ */
+public class MazeBug extends Bug {
+  public Location next;
+  public Location last;
+  public boolean isEnd = false;
+  public Stack<ArrayList<Location>> crossLocation;
+  public Integer stepCount = 0;
+  boolean hasShown = false;// final message has been shown
+
+  /**
+   * Constructs a box bug that traces a square of a given side length
+   */
+  public MazeBug() {
+    setColor(Color.GREEN);
+    last = new Location(0, 0);
+    crossLocation = new Stack<ArrayList<Location>>();
+  }
+
+  /**
+   * Moves to the next location of the square.
+   */
+  public void act() {
+    Location locBefore = getLocation();
+
+    // get the fisrt location to avoid the empty stack exception.
+    if (stepCount == 0) {
+      last = locBefore;
+      ArrayList<Location> currentPath = new ArrayList<Location>();
+      currentPath.add(last);
+      crossLocation.add(currentPath);
+    }
+    boolean willMove = canMove();
+    if (isEnd == true) {
+      // to show step count when reach the goal
+      if (hasShown == false) {
+        String msg = stepCount.toString() + " steps";
+        JOptionPane.showMessageDialog(null, msg);
+        hasShown = true;
+      }
+    } else if (willMove) {
+      // if the last location is a crosspoint, add a new branch
+      if (getValid(locBefore).size() > 1) {
+        ArrayList<Location> newBranch = new ArrayList<Location>();
+        crossLocation.push(newBranch);
+      }
+      move();
+
+      // current location push into stack
+      ArrayList<Location> currentBranch = crossLocation.pop();
+      currentBranch.add(getLocation());
+      crossLocation.push(currentBranch);
+
+      // increase step count when move
+      stepCount++;
+    } else {
+      // if the branch is a dead end, pops out the current location, and Drops a
+      // flower to show that the location has been accessed to.
+      ArrayList<Location> currentBranch = crossLocation.pop();
+      currentBranch.remove(currentBranch.size() - 1);
+      if (currentBranch.size() > 0) {
+        crossLocation.push(currentBranch);
+      }
+      goBack();
+      stepCount++;
+    }
+  }
+
+  /**
+   * Find all positions that can be move to.
+   * 
+   * @param loc
+   *          the location to detect.
+   * @return List of positions. only the empty location and the destination will
+   *         be added.
+   */
+  public ArrayList<Location> getValid(Location loc) {
+    Grid<Actor> gr = getGrid();
+    if (gr == null) {
+      return null;
+    }
+    ArrayList<Location> valid = new ArrayList<Location>();
+    int directions[] = { Location.AHEAD, Location.LEFT, Location.RIGHT,
+        Location.HALF_CIRCLE };
+
+    for (int d : directions) {
+      Location neighbor = loc.getAdjacentLocation(getDirection() + d);
+      if (gr.isValid(neighbor)) {
+        Actor neighborActor = gr.get(neighbor);
+        if (isDestination(neighbor)) {
+          // if the destination is find, move to it
+          valid.removeAll(valid);
+          valid.add(neighbor);
+          return valid;
+        } else if (neighborActor == null) {
+          valid.add(neighbor);
+        }
+      }
+    }
+
+    return valid;
+  }
+
+  private boolean isDestination(Location loc) {
+    Actor locActor = getGrid().get(loc);
+    return (locActor instanceof Rock && locActor.getColor().equals(Color.RED));
+  }
+
+  /**
+   * Tests whether this bug can move forward into a location that is empty or
+   * contains a flower.
+   * 
+   * @return true if this bug can move.
+   */
+  public boolean canMove() {
+    ArrayList<Location> nexts = getValid(getLocation());
+    if (nexts.size() > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Moves the bug forward, putting a flower into the location it previously
+   * occupied.
+   */
+  public void move() {
+    Grid<Actor> gr = getGrid();
+    if (gr == null) {
+      return;
+    }
+
+    // get the next location to move to randomly.
+    Location loc = getLocation();
+    ArrayList<Location> nexts = getValid(loc);
+    Random rand = new Random();
+    int nextNum = rand.nextInt(nexts.size());
+    next = nexts.get(nextNum);
+
+    if (gr.isValid(next)) {
+      setDirection(getLocation().getDirectionToward(next));
+      if (isDestination(next)) {
+        isEnd = true;
+      }
+      moveTo(next);
+    } else {
+      removeSelfFromGrid();
+    }
+    Flower flower = new Flower(getColor());
+    flower.putSelfInGrid(gr, loc);
+  }
+
+  private void goBack() {
+    Grid<Actor> gr = getGrid();
+    Location loc = getLocation();
+    ArrayList<Location> currentBranch = crossLocation.peek();
+    Location lastLoc = currentBranch.get(currentBranch.size() - 1);
+
+    setDirection(getLocation().getDirectionToward(lastLoc));
+    moveTo(lastLoc);
+    Flower flower = new Flower(getColor());
+    flower.putSelfInGrid(gr, loc);
+  }
+
+  public Stack<ArrayList<Location>> getCross() {
+    return crossLocation;
+  }
+}
+
